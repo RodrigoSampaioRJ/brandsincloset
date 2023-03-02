e6f541cc56c888bc0eab60b457fe4f376d6dc0e6
RodrigoSampaioRJ <digo.15rj@gmail.com>
2023-03-02 21:33:10 +0000

    Refactor wp-config3

--- wp-config.php
+++ wp-config.php
@@ -19,17 +19,24 @@
  */
 
 // ** Database settings - You can get this info from your web host ** //
-if(isset($_ENV[`CLEARDB_DATABASE_URL`])) {
-    $db = parse_url($_ENV[`CLEARDB_DATABASE_URL`]);
-    define(‘DB_NAME’, trim($db[`path`],`/`));
-    define(‘DB_USER’, $db[`user`]);
-    define(‘DB_PASSWORD’, $db[`pass`]);
-    define(‘DB_HOST’, $db[`host`]);
-    define(‘DB_CHARSET’, ‘utf8’);
-    define(‘DB_COLLATE’, ‘’);
-} else {
-    die("No Database credentials!");
-}
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'bvywxjll337mzy6y' );
+
+/** Database username */
+define( 'DB_USER', 'v7anb4syvoqoh8do' );
+
+/** Database password */
+define( 'DB_PASSWORD', 'oiannuojcuh38n8c' );
+
+/** Database hostname */
+define( 'DB_HOST', 'd3y0lbg7abxmbuoi.chr7pe7iynqr.eu-west-1.rds.amazonaws.com');
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
 /**#@+
  * Authentication unique keys and salts.
  *
